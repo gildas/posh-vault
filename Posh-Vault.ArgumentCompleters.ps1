29e25f017112aebc981a125248e18deb71d1c08d
Gildas Cherruel <gildas.cherruel@inin.com>
2016-09-12 14:30:49 +0900

    Removed TabExpansionPlusPlus dependencies

--- Posh-Vault.ArgumentCompleters.ps1
+++ Posh-Vault.ArgumentCompleters.ps1
@@ -1,39 +1,68 @@
-function ResourceCompletion
-{
+$ArgumentCompleter = @{
+  CommandName = @('Get-VaultCredential',
+                  'Set-VaultCredential',
+                  'Remove-VaultCredential'
+                 );
+  ParameterName = 'Resource';
+  ScriptBlock   = {
+    <#
+    .SYNOPSIS
+    Auto-complete the -Resource parameter value for Vault Powershell Cmdlets.
+    #>
     Param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter)
 
     $vault = New-Object Windows.Security.Credentials.PasswordVault
-    $vault.retrieveAll() | Where Resource -like "$wordToComplete*" | ForEach {
-      New-CompletionResult -CompletionText $_.Resource -ToolTip "UserName: $($_.UserName)"
+    return $vault.retrieveAll() | Where Resource -like "$wordToComplete*" | ForEach {
+      New-Object -TypeName System.Management.Automation.CompletionResult -ArgumentList @(
+        $_.Resource,
+        ("{0} ({1})" -f $_.Resource, $_.UserName),
+        [System.Management.Automation.CompletionResultType]::ParameterValue,
+        ("UserName: {0}" -f $_.UserName)
+      )
     }
+  }
 }
+Write-Verbose "Registering Argument Completer for $($ArgumentCompleter.ParameterName)"
+Microsoft.Powershell.Core\Register-ArgumentCompleter @ArgumentCompleter
 
-function UserCompletion
-{
+$ArgumentCompleter = @{
+  CommandName = @('Get-VaultCredential',
+                  'Set-VaultCredential',
+                  'Remove-VaultCredential'
+                 );
+  ParameterName = 'User';
+  ScriptBlock   = {
+    <#
+    .SYNOPSIS
+    Auto-complete the -User parameter value for Vault Powershell Cmdlets.
+    #>
     Param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameter)
 
     $vault    = New-Object Windows.Security.Credentials.PasswordVault
     $resource = $fakeBoundParameter['Resource']
     if ($resource)
     {
-      $vault.retrieveAll() | Where { $_.Resource -eq $resource -and $_.UserName -like "*$wordToComplete*" } | ForEach {
-        New-CompletionResult -CompletionText $_.UserName
+      return $vault.retrieveAll() | Where { $_.Resource -eq $resource -and $_.UserName -like "*$wordToComplete*" } | ForEach {
+        New-Object -TypeName System.Management.Automation.CompletionResult -ArgumentList @(
+          $_.UserName,
+          ("{0} ({1})" -f $_.Resource, $_.UserName),
+          [System.Management.Automation.CompletionResultType]::ParameterValue,
+          ("UserName: {0}" -f $_.UserName)
+        )
       }
     }
     else
     {
-      $vault.retrieveAll() | Where UserName -like "$wordToComplete*" | ForEach {
-        New-CompletionResult -CompletionText $_.UserName
+      return $vault.retrieveAll() | Where UserName -like "$wordToComplete*" | ForEach {
+        New-Object -TypeName System.Management.Automation.CompletionResult -ArgumentList @(
+          $_.UserName,
+          ("{0} ({1})" -f $_.Resource, $_.UserName),
+          [System.Management.Automation.CompletionResultType]::ParameterValue,
+          ("UserName: {0}" -f $_.UserName)
+        )
       }
     }
+  }
 }
-
-if (Get-Command Register-ArgumentCompleter -ErrorAction Ignore)
-{
-  Register-ArgumentCompleter -Command Get-VaultCredential    -Parameter Resource -ScriptBlock $function:ResourceCompletion
-  Register-ArgumentCompleter -Command Get-VaultCredential    -Parameter User     -ScriptBlock $function:UserCompletion
-  Register-ArgumentCompleter -Command Set-VaultCredential    -Parameter Resource -ScriptBlock $function:ResourceCompletion
-  Register-ArgumentCompleter -Command Set-VaultCredential    -Parameter User     -ScriptBlock $function:UserCompletion
-  Register-ArgumentCompleter -Command Remove-VaultCredential -Parameter Resource -ScriptBlock $function:ResourceCompletion
-  Register-ArgumentCompleter -Command Remove-VaultCredential -Parameter User     -ScriptBlock $function:UserCompletion
-}
+Write-Verbose "Registering Argument Completer for $($ArgumentCompleter.ParameterName)"
+Microsoft.Powershell.Core\Register-ArgumentCompleter @ArgumentCompleter
